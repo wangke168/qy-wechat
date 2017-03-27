4a3ed7f3415de05ba6a2b889c658e131baa55682
wangke168 <wangke168@gmail.com>
2017-03-27 15:57:27 +0800

    test

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -19,6 +19,7 @@ class TokenController extends Controller
             'token'=>'yz43hRyIDGFUdQy3qtaZ0',	//填写应用接口的Token
             'encodingaeskey'=>'Eu6T9BjvcKn3m2s2DR87uCAE1M4tnbrqXdJ6nTb7DMh',//填写加密用的EncodingAESKey
             'appid'=>'wx6bb8b192d1dcfe19',	//填写高级调用功能的appid
+            'appsecret'=>'H4AyKWaEQiUJg7fQ5abwqrTF_QbjlnzJ8AZlPC_Ll9CiAwote4G-mXOE6C9YDNtj',
             'debug'=>false,
             'logcallback'=>'logg',
             'agentid' => '8', //应用的id
