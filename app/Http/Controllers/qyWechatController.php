5de45e5a31d3b9b535318ef6408bd55de3114f10
wangke168 <wangke168@gmail.com>
2017-03-28 13:55:07 +0800

    test

--- app/Http/Controllers/qyWechatController.php
+++ app/Http/Controllers/qyWechatController.php
@@ -28,7 +28,7 @@ class qyWechatController extends Controller
             'encodingaeskey'=>'Eu6T9BjvcKn3m2s2DR87uCAE1M4tnbrqXdJ6nTb7DMh',//填写加密用的EncodingAESKey
             'appid'=>'wx6bb8b192d1dcfe19',	//填写高级调用功能的appid
             'appsecret'=>'H4AyKWaEQiUJg7fQ5abwqrTF_QbjlnzJ8AZlPC_Ll9CiAwote4G-mXOE6C9YDNtj',
-            'debug'=>false,
+            'debug'=>true,
             'logcallback'=>'logg',
             'agentid' => '8', //应用的id
 
