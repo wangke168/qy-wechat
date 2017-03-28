e0360e7c2e863f13abb3cf93940662c41d0bb323
wangke168 <wangke168@gmail.com>
2017-03-28 13:53:56 +0800

    test

--- app/Http/Controllers/qyWechatController.php
+++ app/Http/Controllers/qyWechatController.php
@@ -35,7 +35,7 @@ class qyWechatController extends Controller
         );
 //        logg("GET参数为：\n".var_export($_GET,true));
         $weObj = new \Wechat($options);
-
+        $weObj->valid();
         /*
         $f = $weObj->getRev()->getRevFrom();	//获取发送者微信号
         $t = $weObj->getRevType();				//获取发送的类型
@@ -51,16 +51,16 @@ class qyWechatController extends Controller
         $t = $weObj->getRevType();				//获取发送的类型
         $d = $weObj->getRevData();				//获取发送的data
         $c = $weObj->getRevContent();			//获取发送的内容
-        if ($t=="text")
-        {
+//        if ($t=="text")
+//        {
 //            $c = $weObj->getRevContent();			//获取发送的内容
 //            $weObj->text("你好！来自星星的：")->reply();
             $c = $weObj->getRevContent();			//获取发送的内容
             $weObj->news($this->Check_tecket($c))->reply();
 
-        }
+//        }
 //logg("-----------------------------------------");
-        $weObj->valid();
+//        $weObj->valid();
 
 
     }
