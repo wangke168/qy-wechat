c45ff17ca8fafd609544022820a92aabf551d68a
wangke168 <wangke168@gmail.com>
2017-04-05 15:08:45 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::any('/test', 'qyWechatController@test');
+Route::any('/test', 'JianPiao\JianPiaoController@index');
 
 Route::any('/tglm', 'qyWechatController@tglm');
 
