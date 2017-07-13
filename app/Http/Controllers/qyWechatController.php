dec7431388f60e9ad6536ab642531e30f3cc60fc
wangke168 <wangke168@gmail.com>
2017-07-13 11:40:11 +0800

    test

--- app/Http/Controllers/qyWechatController.php
+++ app/Http/Controllers/qyWechatController.php
@@ -59,7 +59,8 @@ class qyWechatController extends Controller
 //            $c = $weObj->getRevContent();			//获取发送的内容
 //           $weObj->text("你好！来自星星的：")->reply();
 //            $c = $weObj->getRevContent();			//获取发送的内容
-            $weObj->news($this->Check_tecket($c))->reply();
+//            $weObj->news($this->Check_tecket($c))->reply();
+            $weObj->sendMessage($this->Check_tecket($c));
 
         }
 //logg("-----------------------------------------");
